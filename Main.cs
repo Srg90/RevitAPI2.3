8fc180265cb05b7fabe4e8b9a51b056db0f65284
Srg90 <usikov90@mail.ru>
2022-02-19 12:19:53 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI2._3
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<FamilyInstance> Finstances = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Columns)
+                .WhereElementIsNotElementType()
+                .Cast<FamilyInstance>()
+                .ToList();
+
+            TaskDialog.Show("Colomns count", Finstances.Count.ToString());
+
+            return Result.Succeeded;
+        }
+    }
+}
